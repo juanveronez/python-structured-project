847aef5be68edf8e59fae8fb68e118e3064ca92b
Juan Veronez <juan.monteirov@gmail.com>
2025-02-03 17:10:28 -0300

    código de ETL finalizado

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,8 @@
 from pipeline.extract import extract_from_excels_folder
+from pipeline.transform import concat_dataframes
+from pipeline.load import load_to_excel
 
-df_list = extract_from_excels_folder('data', 'input')
-print(df_list)
+if __name__ == '__main__':
+  df_list = extract_from_excels_folder('data', 'input')
+  df = concat_dataframes(df_list)
+  load_to_excel(df, 'output', path=['data', 'output'])
---
+++ src/pipeline/transform.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def concat_dataframes(dataframes: list[pd.DataFrame]) -> pd.DataFrame:
+  """
+  Função que transforma uma lista de DataFrames em um DataFrame unido.
+  """
+  concated_df = pd.concat(dataframes, ignore_index=True)
+  return concated_df
+
