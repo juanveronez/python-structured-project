3f2261b55bff639852a6fb1c54727e140dee3904
Juan Veronez <juan.monteirov@gmail.com>
2025-02-04 12:11:38 -0300

    Add print to alert the end of the ETL process

--- src/__main__.py
+++ src/__main__.py
@@ -6,3 +6,4 @@ if __name__ == "__main__":
     df_list = extract_from_excels_folder(path=["data", "input"])
     df = concat_dataframes(df_list)
     load_to_excel(df, "output", path=["data", "output"])
+    print("ETL done!")
